3d7c0e9e7dc712e4b796b78fb6c7df98b8eade56
BRIGHT <BRIGHT@DESKTOP-SFUH5C7>
2023-10-08 14:48:42 +0300

    Add project files.

---
+++ AspNetCoreMvc_ETicaret_WebMvcUI/Controllers/AnaSayfaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvc_ETicaret_WebMvcUI.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult SSS()
+        {
+            return View();
+        }
+		public IActionResult Contact()
+		{
+			return View();
+		}
+	}
+   
+}
